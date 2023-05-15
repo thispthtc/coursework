1307c6d348eab6f768efe69d6ddc712c58dd77e0
p-thtc <pathetiiiiic@gmail.com>
2023-05-15 20:55:52 +0700

    add bg to slider

--- resources/js/components/section/menu/MenuSlider.vue
+++ resources/js/components/section/menu/MenuSlider.vue
@@ -1,15 +1,30 @@
 <template>
     <div class="menu-slider">
+        <img class="m-bg" :src="getImageUrl('bg-slider.png')" alt="">
         <MenuSliderItem/>
+        <MenuSliderNav/>
     </div>
 </template>
 
 <script>
 import MenuSliderItem from "@/components/section/menu/MenuSliderItem.vue";
+import MenuSliderNav from "@/components/section/menu/MenuSliderNav.vue";
 
-export default {}
+export default {
+    components: {MenuSliderItem, MenuSliderNav},
+    methods: {
+        getImageUrl: filename => {
+            return import.meta.env.VITE_APP_IMAGE_PATH + filename
+        }
+    }
+}
 </script>
 
-<style scoped>
-
+<style>
+    .m-bg {
+        position: absolute;
+        left: 0;
+        right: 0;
+        z-index: -1;
+    }
 </style>
