96d74ab0bbfbd7628870a614968d7e3b3a874b56
p-thtc <pathetiiiiic@gmail.com>
2023-05-18 19:38:02 +0700

    create menu in db

---
+++ database/migrations/2023_05_18_114435_create_menus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_category');
+            $table->string('name', 20);
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('weight');
+            $table->string('src_img', 50);
+            $table->timestamps();
+
+            $table->foreign('id_category')
+                  ->references('id')
+                  ->on('menu_categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
