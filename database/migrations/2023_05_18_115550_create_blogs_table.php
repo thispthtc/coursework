6e012897fa2171c190110273da353089ab1962df
p-thtc <pathetiiiiic@gmail.com>
2023-05-18 19:38:25 +0700

    create blog in db

---
+++ database/migrations/2023_05_18_115550_create_blogs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_category');
+            $table->date('date');
+            $table->string('title', 20);
+            $table->text('description')->nullable();
+            $table->string('src_img');
+            $table->timestamps();
+
+
+            $table->foreign('id_category')
+                  ->references('id')
+                  ->on('blog_categories')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
