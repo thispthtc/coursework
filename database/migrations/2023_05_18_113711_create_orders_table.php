8bc910ae9116dbf068037bab289227970c024e6a
p-thtc <pathetiiiiic@gmail.com>
2023-05-18 19:37:30 +0700

    create order in db

---
+++ database/migrations/2023_05_18_113711_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name', 50);
+            $table->string('number', 12);
+            $table->unsignedInteger('guest_count');
+            $table->date('date');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
