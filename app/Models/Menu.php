d6a6ccc08fcffbfa3ef8aec395b43e55021bfefb
p-thtc <pathetiiiiic@gmail.com>
2023-05-18 20:45:33 +0700

    add link

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    public function category(): HasOne
+    {
+        return $this->hasOne(MenuCategory::class);
+    }
 }
--- app/Models/MenuCategory.php
+++ app/Models/MenuCategory.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class MenuCategory extends Model
 {
     use HasFactory;
+
+    public function menus(): HasMany
+    {
+        return $this->hasMany(Menu::class);
+    }
 }
