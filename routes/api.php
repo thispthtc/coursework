680a62b67e87c79b81982f52b78189c3917b2c75
thispthtc <pathetiiiiic@gmail.com>
2023-05-19 15:06:19 +0700

    make api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\HomeSliderController;
+use App\Http\Controllers\Api\MenuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources([
-    'home' => HomeSliderController::class,
-]);
+Route::get('/menu', [MenuController::class, 'index']);
+Route::get('/menu/{category}', [MenuController::class, 'show']);
